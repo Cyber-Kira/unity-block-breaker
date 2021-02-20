622e4ddad7c6898f581428c99e868b734efc819e
Cyber-Kira <kkoloskov02@gmail.com>
2021-02-20 23:16:50 +0300

    Load next scene when every block is destroyed

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -17,7 +17,13 @@ public class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 0.5f);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -7,6 +7,13 @@ public class Level : MonoBehaviour
 
     [SerializeField] int breakableBlocks; // serialized for debuggin purposes
 
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
@@ -15,5 +22,9 @@ public class Level : MonoBehaviour
     public void BlockDestroyed()
     {
         breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
